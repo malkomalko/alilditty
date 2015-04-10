98b5893f6e36d7b7882bd4d9eb88a68a1b56afb4
Robert Malko <robmalko@gmail.com>
2015-04-10 14:32:36 -0400

    add react-hotkeys to select tracks by #

--- app/components/Application/index.jsx
+++ app/components/Application/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import {Events, EventNames} from '../../events'
 import Grid from '../Grid'
+import {HotKeys} from 'react-hotkeys';
 
 require('./style.sass')
+
+const keyMap = {
+  'selectTrack': ['1', '2', '3', '4', '5', '6', '7', '8'],
+}
 
 export default class Application extends React.Component {
   constructor(props) {
@@ -26,9 +31,11 @@ export default class Application extends React.Component {
     this.setupEvents()
   }
   render() {
-    return <div className="Application" onWheel={this.onWheel}>
-      <Grid {...this.state} rows={2} cols={4} />
-    </div>
+    return <HotKeys keyMap={keyMap} handlers={this.handlers()}>
+      <div className="Application" onWheel={this.onWheel}>
+        <Grid {...this.state} rows={2} cols={4} />
+      </div>
+    </HotKeys>
   }
   setupEvents() {
     this.state.events.on(EventNames.STATE_SELECTED_TRACK, (index) => {
@@ -37,5 +44,16 @@ export default class Application extends React.Component {
   }
   onWheel(e) {
     e.preventDefault()
+  }
+  handlers() {
+    return {
+      'selectTrack': (e) => {
+        var trackNumber = e.keyCode - 49
+        if (this.state.selectedTrack === trackNumber) {
+          trackNumber = null
+        }
+        this.setState({ selectedTrack: trackNumber })
+      },
+    }
   }
 }
