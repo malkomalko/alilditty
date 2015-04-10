4e9d59438a15eae7dbf68abe4e4cf77c1a12db9d
Robert Malko <robmalko@gmail.com>
2015-04-10 12:19:49 -0400

    add Track class

--- app/components/Grid/index.jsx
+++ app/components/Grid/index.jsx
@@ -3,15 +3,27 @@ import React from 'react';
 require('./style.sass');
 
 var gridColors = [
-  '#F58E99',
-  '#6AC6D6',
-  '#FFEE81',
-  '#FCC886',
-  '#88CCAC',
-  '#A3A3C8',
-  '#6AA7CE',
-  '#A09C97',
+  '#F58E99', '#6AC6D6', '#FFEE81', '#FCC886',
+  '#88CCAC', '#A3A3C8', '#6AA7CE', '#A09C97',
 ]
+
+class Track extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <div className="Track" style={this.trackStyles(this.props.index)}>
+
+    </div>
+  }
+  trackStyles(i) {
+    return {
+      backgroundColor: gridColors[i],
+      height: (100 / this.props.rows) + '%',
+      width: (100 / this.props.cols) + '%',
+    }
+  }
+}
 
 export default class Grid extends React.Component {
   constructor(props) {
@@ -21,19 +33,14 @@ export default class Grid extends React.Component {
   }
   render() {
     const tracks = this.state.tracks.map((track, i) => {
-      return <div className="Track" key={i} style={this.trackStyles(i)}>
-      </div>
+      return <Track key={i} index={i}
+        track={this.state.tracks[i]}
+        cols={this.props.cols}
+        rows={this.props.rows} />
     })
     return <div className="Grid">
       {tracks}
     </div>
-  }
-  trackStyles(i) {
-    return {
-      backgroundColor: gridColors[i],
-      height: (100 / this.props.rows) + '%',
-      width: (100 / this.props.cols) + '%',
-    }
   }
 }
 
