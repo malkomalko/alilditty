5e81a80a701368c9a4e0186a3c18ea28f2d258e6
Robert Malko <robmalko@gmail.com>
2015-04-16 13:04:15 -0400

    create custom slider for mixer strip handle

--- app/components/Mixer/index.jsx
+++ app/components/Mixer/index.jsx
@@ -2,21 +2,60 @@ import React from 'react'
 import {Events, EventNames} from '../../events'
 
 require('./style.sass')
+var _ = require('lodash')
 
 export default class Mixer extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {}
+    this.mouseState = {}
 
     this.setupEvents()
   }
   render() {
     return <div className="Mixer">
+      {this.mixerStrips()}
     </div>
+  }
+  mixerStrips() {
+    return _.map(this.props.tracks, (track, index) => {
+      return <div className="MixerStrip" key={"mixerStrip-" + index}
+        onMouseDown={this.onMouseDown.bind(this, index)}
+        onMouseMove={this.onMouseMove.bind(this, index)}
+        onMouseUp={this.onMouseUp.bind(this, index)}>
+        <div className="MixerStripHandle" />
+      </div>
+    })
   }
   setupEvents() {
 
+  }
+  onMouseDown(index, e) {
+    e.persist()
+    this.mouseState[index] = true
+  }
+  onMouseMove(index, e) {
+    if (this.mouseState[index]) {
+      e.persist()
+
+      var el;
+      if (e.target.className === 'MixerStrip') {
+        el = e.target
+      } else {
+        el = e.target.parentElement
+      }
+
+      var rect = el.getBoundingClientRect()
+      var base = ~~(rect.height) + ~~(rect.top) - e.clientY
+      var percent = Math.round(base / ~~(rect.height) * 100) + '%'
+
+      el.getElementsByClassName('MixerStripHandle')[0].style.height = percent
+    }
+  }
+  onMouseUp(index, e) {
+    e.persist()
+    this.mouseState[index] = false
   }
   componentDidMount() {
     // called once after initial render => React.findDOMNode(this)
